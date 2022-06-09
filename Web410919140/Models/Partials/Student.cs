71266d1fe3ef6214aebb6cb02bdd4d39b6e1f6d7
Yee <s1091914@gm.pu.edu.tw>
2022-06-09 19:47:41 +0800

    在資料表新增欄位 在資料表更新欄位

--- Web410919140/Models/Partials/Student.cs
+++ Web410919140/Models/Partials/Student.cs
@@ -17,7 +17,7 @@ namespace Web410919140.Models
         [Required(ErrorMessage = "請填寫姓名")]
         [Display(Name = "姓名")]
         [StringLength(5, MinimumLength =2, ErrorMessage ="請輸入2~5個字")]
-        public string Name { get; set; }
+        public string Username { get; set; }
         [Required(ErrorMessage = "請填寫學號")]
         [Display(Name = "學號")]
         [StringLength(10, ErrorMessage = "請輸入1~10個字")]
@@ -26,5 +26,9 @@ namespace Web410919140.Models
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "必填欄位")]
+        [Display(Name = "是否註冊")]
+        public bool IsActive { get; set; }
     }
 }
